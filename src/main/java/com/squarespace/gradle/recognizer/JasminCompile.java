1978b62ef8dd83b43571a32ce1367e643f47c71a
Patrick Hensley <pathensley@gmail.com>
2021-06-02 17:43:41 -0400

    Initial Jasmin compile plugin and example project

---
+++ src/main/java/com/squarespace/gradle/recognizer/JasminCompile.java
@@ -0,0 +1,94 @@
+package com.squarespace.gradle.recognizer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.gradle.api.GradleException;
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.compile.AbstractCompile;
+
+import jasmin.ClassFile;
+
+/**
+ * Assembles Jasmin source into JVM classfile format.
+ */
+public class JasminCompile extends AbstractCompile {
+
+  private List<Path> sourceDirs;
+
+  public void sourceDirs(Set<File> sourceDirs) {
+    this.sourceDirs = sourceDirs.stream()
+        .map(e -> Paths.get(e.getAbsolutePath()))
+        .collect(Collectors.toList());
+  }
+
+  @TaskAction
+  public void compile() {
+    Path destDir = Paths.get(getDestinationDir().getAbsolutePath());
+    for (File file : getSource().getFiles()) {
+
+      // Read source into a Jasmin class file object
+      ClassFile cls = new ClassFile();
+      try (InputStream is = new FileInputStream(file)) {
+        cls.readJasmin(is, file.getName(), true);
+      } catch (Exception e) {
+        throw new GradleException("Error reading Jasmin input file", e);
+      }
+
+      // Construct output path. We use the relative path since we cannot
+      // obtain the package name from the Jasmin class file directly
+      Path sourcePath = relativeSourcePath(file);
+      String baseName = baseName(file.getName());
+      Path outDir = destDir.resolve(sourcePath);
+      makedirs(outDir);
+      String outPath = outDir.resolve(baseName + ".class").toString();
+
+      // Write class file
+      try (OutputStream os = new FileOutputStream(outPath)) {
+        cls.write(os);
+      } catch (Exception e) {
+        throw new GradleException("Error writing class output file", e);
+      }
+    }
+  }
+
+  /**
+   * Ensure the given directory and all of its parents exist or are created.
+   */
+  private void makedirs(Path dir) {
+    File file = dir.toFile();
+    if (!file.exists()) {
+      file.mkdirs();
+    }
+  }
+
+  /**
+   * Return the relative path to the file under one of the source directories.
+   */
+  private Path relativeSourcePath(File file) {
+    Path filePath = Paths.get(file.getAbsolutePath());
+    for (Path dir : sourceDirs) {
+       if (filePath.startsWith(dir)) {
+         return dir.relativize(filePath).getParent();
+       }
+    }
+    return filePath.getParent();
+  }
+
+  /**
+   * Strip the file extension, if any.
+   */
+  private String baseName(String name) {
+    int i = name.lastIndexOf('.');
+    return i == -1 ? name : name.substring(0, i);
+  }
+}
+
---
+++ src/main/java/com/squarespace/gradle/recognizer/JasminPlugin.java
@@ -0,0 +1,75 @@
+package com.squarespace.gradle.recognizer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.gradle.api.Action;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.file.SourceDirectorySet;
+import org.gradle.api.internal.tasks.DefaultSourceSet;
+import org.gradle.api.internal.tasks.DefaultSourceSetOutput;
+import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.tasks.SourceSet;
+
+/**
+ * Gradle plugin for compiling Jasmin assembly source code.
+ */
+public class JasminPlugin implements Plugin<Project> {
+
+  public void apply(Project project) {
+    System.setProperty("file.encoding", "UTF-8");
+
+    // We piggy-back on the Java plugin so make sure it's applied
+    project.getPlugins().apply("java");
+
+    // Loop over the existing Java source sets (e.g. "main", "test")
+    project.getConvention().getPlugin(JavaPluginConvention.class).getSourceSets().all(new Action<SourceSet>() {
+      public void execute(SourceSet javaSourceSet) {
+
+        // Setup naming of our source set
+        final String name = javaSourceSet.getName();
+        final String displayName = ((DefaultSourceSet)javaSourceSet).getDisplayName();
+
+        // Add a "src/{main,test}/jasmin/**/*.j" source set
+        final String sourcePath = Paths.get("src").resolve(name).resolve("jasmin").toString();
+        final SourceDirectorySet jasminSet = project.getObjects().sourceDirectorySet(name, displayName);
+        jasminSet.getFilter().include(new String[] { "**/*.j" });
+        jasminSet.srcDir(sourcePath);
+
+        // Ensure Jasmin sources are visible in the Java source set
+        javaSourceSet.getAllJava().source(jasminSet);
+        javaSourceSet.getAllSource().source(jasminSet);
+
+        // Get references to the build and classes output directories
+        final String buildDir = project.getLayout().getBuildDirectory().get().toString();
+        final Path destinationDir = Paths.get(buildDir).resolve("classes").resolve("jasmin").resolve(name);
+
+        // Create a named task for compiling Jasmin files in the current source set (e.g. "main")
+        final String taskName = javaSourceSet.getCompileTaskName("jasmin");
+
+        // Ensure compilation of Java depends on Jasmin compilation
+        Task javaTask = project.getTasks().getByName(javaSourceSet.getCompileJavaTaskName());
+        javaTask.dependsOn(taskName);
+
+        // Ensure compiled Java classes can see Jasmin classes on the classpath
+        javaSourceSet.setCompileClasspath(javaSourceSet.getCompileClasspath().plus(project.files(destinationDir)));
+
+        // Ensure jar contains Jasmin-compiled classes
+        DefaultSourceSetOutput outputSet = (DefaultSourceSetOutput) javaSourceSet.getOutput();
+        outputSet.addClassesDir(project.getLayout().dir(project.provider(() -> destinationDir.toFile())));
+
+        // Create the Jasmin compile task for this source set
+        project.getTasks().create(taskName, JasminCompile.class, (task) -> {
+          task.setDescription("Compiles the " + name + " Jasmin source");
+          task.setSource(jasminSet);
+          task.setClasspath(javaSourceSet.getCompileClasspath());
+          task.setDestinationDir(destinationDir.toFile());
+          task.sourceDirs(jasminSet.getSrcDirs());
+        });
+
+      }
+    });
+  }
+}
